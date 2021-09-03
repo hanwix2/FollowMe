b46d69dddb3fb0d4abd2d0697c3f31ceadff8dea
Jeonghan-Shin <alexsjh23@gmail.com>
2021-09-03 18:49:37 +0900

    Create Project

---
+++ src/main/java/com/fm/FollowMe/FollowMeApplication.java
@@ -0,0 +1,13 @@
+package com.fm.FollowMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fm/FollowMe/FollowMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fm.FollowMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
